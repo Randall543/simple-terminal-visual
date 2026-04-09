596ea1a7d3e54c365070777a0bba5d798b716f13
art <silentvoice5001@gmail.com>
2026-04-09 02:14:30 -0400

    added flags to test posix lib & it worked great!

--- input-fd.c
+++ input-fd.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(){
+int main(int argc, char* argv[]){
+	if (argc == 1){
+		printf("no flag provied");
+	}
 	char buffer[100];
-	fread(buffer, sizeof buffer, 20, stdin);
+	// 0 is to check for stdin from standard lib
+	if (atoi(argv[1]) == 0){
+		fread(buffer, sizeof buffer, 20, stdin);
+	}
+	// 1 is to check for stdin from posix
+	if (atoi(argv[1]) == 1){
+		read(0, buffer, sizeof buffer);
+	}
 	printf("from stdin: %s", buffer);
 	return 1;
 }
